ce883b369bea8991f9b6863b35f8e6042a29518a
Rosen Dimitrov <rosen.dimitrov@test.com>
2024-04-17 20:32:35 +0300

    new models adde

--- hit_dakwerken/hit_dakwerken/invoice/models.py
+++ hit_dakwerken/hit_dakwerken/invoice/models.py
@@ -1,3 +1,51 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from hit_dakwerken.query.models import Query
+
+UserModel = get_user_model()
+
+
+class Invoice(models.Model):
+    ADDRESS = [
+        'DAKGROEP NAESSENS',
+        'Leenstraat 135'''
+        '9870 Zulte'''
+        'België'
+    ]
+    VAT_NUMBER = 'BE0445298492'
+
+    invoice_number = models.IntegerField(
+        unique=True,
+        primary_key=True,
+    )
+    address = models.CharField(
+        default=ADDRESS,
+        max_length=len(ADDRESS),
+    )
+    invoice_date = models.DateField(
+        auto_now_add=True,
+    )
+    expiry_date = models.DateField(
+        blank=False,
+        null=False,
+    )
+    vat_number = VAT_NUMBER
+
+    # employe = models
+
+    description = models.TextField(
+        blank=True,
+        null=True,
+    )
+    user = models.ForeignKey(
+        UserModel,
+        on_delete=models.DO_NOTHING,
+    )
+    query = models.ForeignKey(
+        Query,
+        on_delete=models.DO_NOTHING,
+        blank=True,
+        null=True,
+    )
+
--- hit_dakwerken/hit_dakwerken/offer_reply/models.py
+++ hit_dakwerken/hit_dakwerken/offer_reply/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from hit_dakwerken.query.models import Query
+
+
+class Offer(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.PositiveIntegerField(
+        null=False,
+        blank=False,
+    )
+    query = models.ForeignKey(
+        Query,
+        on_delete=models.CASCADE,
+    )
